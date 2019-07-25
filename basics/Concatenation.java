20a7eb5413e9d6ef5f4e2e1eb72d8bea4c857ba8
Aiden Onstott <aidenonstott@gmail.com>
2019-07-24 18:33:35 -0700

    Added Concatenation and Arithmetic

---
+++ basics/Concatenation.java
@@ -0,0 +1,15 @@
+public class Concatenation {
+  public static void main(String[] args) {
+    System.out.println("Hello, " + "World!");
+
+    System.out.println(2 + 2);
+
+    int math = 2 + 5;
+
+    String concatenation = "h" + "i";
+
+    System.out.println(math);
+
+    System.out.println(concatenation);
+  }
+}
