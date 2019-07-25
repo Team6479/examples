668c0042ae69ca92e5aff314d7231298d3d674c9
Aiden Onstott <aidenonstott@gmail.com>
2019-07-24 17:12:38 -0700

    Added basic variables example

---
+++ basics/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myAge = 15;
+        System.out.println(myAge);
+
+        // double myAge = 15.5;
+        // System.out.println(myAge);
+
+        // String myFavoriteColor = "Purple";
+        // System.out.println(myFavoriteColor);
+    }
+}
