ce8185c1ae0c46679a86634e43b2820154518f5d
Aiden Onstott <aidenonstott@gmail.com>
2019-07-24 17:23:46 -0700

    Fixed indentation

--- basics/Variables.java
+++ basics/Variables.java
@@ -1,12 +1,12 @@
 public class Variables {
-    public static void main(String[] args) {
-        int myAge = 15;
-        System.out.println(myAge);
+  public static void main(String[] args) {
+    int myAge = 15;
+    System.out.println(myAge);
 
-        // double myAge = 15.5;
-        // System.out.println(myAge);
+    // double myAge = 15.5;
+    // System.out.println(myAge);
 
-        // String myFavoriteColor = "Purple";
-        // System.out.println(myFavoriteColor);
-    }
+    // String myFavoriteColor = "Purple";
+    // System.out.println(myFavoriteColor);
+  }
 }
